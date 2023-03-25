18e6613abd48646beb0781e3899fdd57617e1889
Григорий Алексанян <grigoriy.aleksanyan@nw.rt.ru>
2023-03-25 20:02:09 +0300

    build: добавил новый проект в решение

---
+++ AskQuestion.Core/Constants/UserStringRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace AskQuestion.Core.Constants
+{
+    public static class UserStringRoles
+    {
+        public const string ADMINISTRATORS_ONLY = "Administrator";
+        public const string SPEAKERS_ONLY = "Speaker";
+        public const string ADMINISTRATORS_AND_SPEAKERS = "Administrator,Speaker";
+    }
+}
---
+++ AskQuestion.Core/Enums/UserRole.cs
@@ -0,0 +1,18 @@
+﻿namespace AskQuestion.Core.Enums
+{
+	/// <summary>
+	/// Роль пользователя.
+	/// </summary>
+	public enum UserRole
+	{
+        /// <summary>
+        /// Администратор.
+        /// </summary>
+        Administrator = 1,
+
+        /// <summary>
+        /// Спикер
+        /// </summary>
+        Speaker = 2,
+	}
+}
