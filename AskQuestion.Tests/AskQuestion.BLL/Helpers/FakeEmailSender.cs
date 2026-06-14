8855260838e1cf5a56536051f53163224054c73f
Григорий Алексанян <Grigoriy.Aleksanyan@nw.rt.ru>
2026-06-14 14:27:19 +0300

    test: implement IEmailSender.GetReader in FakeEmailSender

--- AskQuestion.Tests/AskQuestion.BLL/Helpers/FakeEmailSender.cs
+++ AskQuestion.Tests/AskQuestion.BLL/Helpers/FakeEmailSender.cs
@@ -1,3 +1,4 @@
+using System.Threading.Channels;
 using AskQuestion.BLL.Email;
 
 namespace AskQuestion.BLL.Tests.Helpers;
@@ -5,12 +6,20 @@ namespace AskQuestion.BLL.Tests.Helpers;
 public sealed class FakeEmailSender : IEmailSender
 {
     private readonly List<EmailMessage> _messages = new();
+    private readonly Channel<EmailMessage> _channel = Channel.CreateUnbounded<EmailMessage>(new UnboundedChannelOptions
+    {
+        SingleReader = true,
+        SingleWriter = false,
+    });
 
     public IReadOnlyList<EmailMessage> Messages => _messages.AsReadOnly();
 
     public Task EnqueueAsync(EmailMessage message)
     {
         _messages.Add(message);
+        _channel.Writer.TryWrite(message);
         return Task.CompletedTask;
     }
+
+    public ChannelReader<EmailMessage> GetReader() => _channel.Reader;
 }
