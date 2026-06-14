60e26839e69a43ad9552931981890e76560b76e8
Григорий Алексанян <Grigoriy.Aleksanyan@nw.rt.ru>
2026-06-14 13:55:00 +0300

    test: add EmailSenderTests

---
+++ AskQuestion.Tests/AskQuestion.BLL/Email/EmailSenderTests.cs
@@ -0,0 +1,26 @@
+using AskQuestion.BLL.Email;
+using FluentAssertions;
+
+namespace AskQuestion.BLL.Tests.Email
+{
+    public class EmailSenderTests
+    {
+        [Fact]
+        public async Task EnqueueAsync_WritesMessageToChannel()
+        {
+            var sender = new EmailSender();
+            var message = new EmailMessage
+            {
+                ToEmail = "to@test.com",
+                ToName = "To",
+                Subject = "Subject",
+                HtmlBody = "Body",
+            };
+
+            await sender.EnqueueAsync(message);
+
+            var read = await sender.GetReader().ReadAsync();
+            read.Should().BeEquivalentTo(message);
+        }
+    }
+}
